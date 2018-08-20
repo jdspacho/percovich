fc75f376c4d1b90ba61489bd2725136367bccccb
jonathan <i1620460@continental.edu.pe>
2018-08-20 17:55:26 -0500

    mas

--- src/Percovich/Percovich/Models/User.cs
+++ src/Percovich/Percovich/Models/User.cs
@@ -9,10 +9,46 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RestSharp;
 
 namespace Percovich.Models
 {
     class User
     {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+        public User()
+        {
+        }
+        public static List<User> sync()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://reqres.in/");
+
+            var request = new RestRequest();
+            request.Resource = "api/users";
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+                UserRest rootObject = deserial.Deserialize<UserRest>(response);
+                return rootObject.data;
+            }
+            else
+            {
+                UserRest rootObject = new UserRest();
+                return rootObject.data;
+            }
+        }
+    }
+    public class UserRest
+    {
+        public List<User> data { get; set; }
+    }
+}
+
     }
 }
--- src/Percovich/Percovich/Models/tratamiento.cs
+++ src/Percovich/Percovich/Models/tratamiento.cs
@@ -9,10 +9,57 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RestSharp;
 
 namespace Percovich.Models
 {
     class tratamiento
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string color { get; set; }
+        public static List<tratamiento> local()
+        {
+            return new List<tratamiento>() {
+                new tratamiento(){name = "Uno" },
+                new tratamiento(){name = "Dos" },
+                new tratamiento(){name = "Tres" },
+                new tratamiento(){name = "Cuatro" },
+                new tratamiento(){name = "Cinco" },
+                new tratamiento(){name = "Seis" },
+                new tratamiento(){name = "Siete" },
+                new tratamiento(){name = "Ocho" },
+            };
+        }
+
+        public static List<tratamiento> sync()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://reqres.in/");
+
+            var request = new RestRequest();
+            request.Resource = "api/products";
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+                ProductRest rootObject = deserial.Deserialize<ProductRest>(response);
+                return rootObject.data;
+            }
+            else
+            {
+                ProductRest rootObject = new ProductRest();
+                return rootObject.data;
+            }
+        }
+    }
+    public class ProductRest
+    {
+        public List<tratamiento> data { get; set; }
+    }
+}
     }
 }
